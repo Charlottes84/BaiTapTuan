df5216b9e8f9ee6443979f867f5c92297dfb06f9
Charlottes84 <hungnguyenduc023@gmail.com>
2024-04-19 10:49:36 +0700

    file c++

--- week4/bai5.cpp
+++ week4/bai5.cpp
@@ -1,62 +1,37 @@
-#include <bits/stdc++.h>
-#define taskname ""
-#define ll long long
+#include<iostream>
 
 using namespace std;
-const int N = 1e3 + 7;
 
-int a[N][N], n, m;
+const int N = 5e2 + 1;
 
-void Solve()
+void create_spiral_board(int m, int n)
 {
-	cin >> m >> n;
-	int h1 = 1, h2 = m, c1 = 1, c2 = n;
-	int cur = 1;
-	while(h1 <= h2)
-	{
-		for(int i = 1; i <= c2; i++)
-		{
-			a[h1][i] = cur;
-			cur++; 
-		}
-		h1++;
-		for(int i = h1; i <= h2; i++)
-		{
-			a[i][c2] = cur;
-			cur++;
-		}
-		c2--;
-		for(int i = c2; i >= 1; i--)
-		{
-			a[h2][i] = cur;
-			cur++;
-		}
-		h2--;
-		for(int i = h2; i >= h1; i--)
-		{
-			a[i][c1] = cur;
-			cur++;
-		}
-		c1++;
+	int a[N][N];
+	int dong = m, cot = n, k = 1, p = 0, i, j;
+	while(k <= m*n){
+		for(i = p; i < cot; i++) 
+			a[p][i] = k++;
+		for(i = p+1; i < dong; i++) 
+			a[i][cot-1] = k++;
+		if ( p != dong-1){
+			for(i = cot-2; i >=p; i--) 
+				a[dong-1][i] = k++;
+		}	
+		if(p!=cot-1){
+			for(i = dong-2; i > p; i--) 
+				a[i][p] = k++;
+		}	
+		p++;dong --; cot --;
 	}
-	for(int i = 1; i <= m; i++)
-	{
-		for(int j = 1; j <= n; j++)
+	for(i = 0; i < m; i++){
+		for(j = 0; j < n; j++)
 			cout << a[i][j] << " ";
 		cout << endl;
 	}
 }
 
-int main()
-{
-//	if(ifstream(taskname".inp")){
-//	freopen(taskname".inp", "r", stdin);
-//	freopen(taskname".out", "w", stdout);
-//	}
-	ios_base :: sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	
-	Solve();
-	
-	return 0;
+int main() {
+	int m, n;
+	cin >> m >> n;
+	create_spiral_board(m, n);
 }
